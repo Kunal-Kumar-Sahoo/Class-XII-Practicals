e7695c722f2c53121bd4e1b6085131a39d8e0847
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2021-05-18 15:37:45 +0530

    Write a recursive pyhton program to test if a string is palindrome or not.

---
+++ program8.py
@@ -0,0 +1,20 @@
+# Write a recursive python program to test if a string is palindrome or not.
+
+def check_palindrome(string):
+    if len(string) == 1:
+        return True
+    if string[0] == string[-1]:
+        return check_palindrome(string[1:-1])
+    return False
+
+
+if __name__ == "__main__":
+    string = input("Enter a string: ")
+
+    if string == '':
+        print("Empty string !")
+    else:
+        if check_palindrome(string):
+            print(f"{string} is a palindrome")
+        else:
+            print(f"{string} is not a palindrome")
